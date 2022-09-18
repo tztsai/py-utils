c6d248daa0b07d44dab409f24b4d52aed7ffda4f
TZCai <13818704679@163.com>
2022-09-18 22:49:32 +0200

    add new decos

--- my_utils/deco.py
+++ my_utils/deco.py
@@ -1,19 +1,26 @@
+import sys
 from time import time
-from functools import update_wrapper, wraps
+from functools import update_wrapper, wraps, partial
+from collections import defaultdict
+
 # When you use a decorator to define the function, the original function is
-# shadowed by the function returned by the decorator. This causes that if you
+# replaced by the function returned by the decorator. This causes that if you
 # call the "help" function upon the decorated function, the info it displays is
 # that of the returned function of the decorator. "update_wrapper" is used to 
 # deal with this problem.
 # Use the built-in function "help" to see the docstrings.
-
-
 def decorator(dec):
-    """Makes the decorated decorator such that when it decorates a function, the
-    decorated function looks like the original function."""
-    def _dec(f):
+    """ Makes the decorated decorator such that when it decorates a function, the
+    decorated function looks like the original function.
+    Additionally, the decorated decorator can be partially initialized with provided
+    arguments, when the first of them is non-callable. """
+    def _dec(*args, **kwargs):
         "This will be displayed if the second update_wrapper is absent."
-        return update_wrapper(dec(f), f)  # update_wrapper returns the wrapper
+        assert args, "no arguments provided to the decorator"
+        if not callable(args[0]):
+            return partial(dec, *args, **kwargs)  # return another decorator
+        assert len(args) == 1 and not kwargs, "too many arguments provided to the decorator"
+        return update_wrapper(dec(args[0]), args[0])  # update_wrapper returns the wrapper
     return update_wrapper(_dec, dec)
 
 
@@ -71,3 +78,64 @@ def override(cls, name=None):
         setattr(cls, name, method)
         return method
     return deco
+
+
+class TraceLocals:
+    """ A decorator to trace the local variables of a function after it returns. """
+    
+    def __init__(self, func):
+        self._locals = {}
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        def tracer(frame, event, arg):
+            if event == 'return':
+                self._locals = frame.f_locals.copy()
+
+        # tracer is activated on next call, return or exception
+        sys.setprofile(tracer)
+        try:
+            # trace the function call
+            res = self.func(*args, **kwargs)
+        finally:
+            # disable tracer and replace with old one
+            sys.setprofile(None)
+        return res
+
+    def clear_locals(self):
+        self._locals = {}
+
+    @property
+    def locals(self):
+        return self._locals
+
+
+class Profile:
+    debug_counts, debug_times = defaultdict(int), defaultdict(float)
+
+    @classmethod
+    def print_debug_exit(cls):
+        print('\n{}  COUNT --- TIME COST'.format('-' * 47))
+        for name, _ in sorted(cls.debug_times.items(), key=lambda x: -x[1]):
+            print(f"{name:<45} : {cls.debug_counts[name]:>6} {cls.debug_times[name]:>10.2f} ms")
+
+    def __init__(self, name=''):
+        self.name = name
+
+    def __enter__(self):
+        self.st = time.time()
+
+    def __exit__(self, *_):
+        et = (time.time() - self.st) * 1000.
+        self.debug_counts[self.name] += 1
+        self.debug_times[self.name] += et
+        # debug(f"{self.name:>20} : {et:>7.2f} ms")
+
+@decorator
+def timed(fn, name=None):
+    if name is None:
+        name = fn.__qualname__
+    def wrapper(*args, **kwds):
+        with Profile(name):
+            return fn(*args, **kwds)
+    return wrapper
