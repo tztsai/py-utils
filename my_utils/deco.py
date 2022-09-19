657fad18d206d800bdeb988469adbf70c12bcc12
TZCai <13818704679@163.com>
2022-09-19 11:25:14 +0200

    update

--- my_utils/deco.py
+++ my_utils/deco.py
@@ -1,4 +1,5 @@
 import sys
+import inspect
 from time import time
 from functools import update_wrapper, wraps, partial
 from collections import defaultdict
@@ -16,12 +17,29 @@ def decorator(dec):
     arguments, when the first of them is non-callable. """
     def _dec(*args, **kwargs):
         "This will be displayed if the second update_wrapper is absent."
-        if not callable(args[0]):
+        if not args or not callable(args[0]):
             return partial(dec, *args, **kwargs)  # return another decorator
-        assert len(args) == 1, "too many arguments provided to the decorator"
-        assert not kwargs, "keyword arguments are not allowed for a decorator"
+        assert len(args) == 1 and not kwargs
         return update_wrapper(dec(args[0]), args[0])
     return update_wrapper(_dec, dec)  # update_wrapper returns the wrapper
+
+
+@decorator
+def main(fn):
+    """Call fn with command line arguments.  Used as a decorator.
+
+    The main decorator marks the function that starts a program. For example,
+
+    @main
+    def my_run_function():
+        # function body
+
+    Use this instead of the typical __name__ == "__main__" predicate.
+    """
+    if inspect.stack()[1][0].f_locals['__name__'] == '__main__':
+        args = sys.argv[1:]  # Discard the script name from command line
+        fn(*args)  # Call the main function
+    return fn
 
 
 @decorator
@@ -83,27 +101,63 @@ def override(cls, name=None):
 class trace_locals:
     """ A decorator to trace the local variables of a function after it returns. """
     
-    def __init__(self, func):
-        self._locals = {}
-        self._func = func
+    def __init__(self, func, modifiers: dict = None):
+        self.vars = {}
+        self.__fn = func
+        self.__co = func.__code__
+        self._mod = modifiers
 
     def __call__(self, *args, **kwargs):
         def tracer(frame, event, arg):
-            if event == 'return':
-                self._locals = frame.f_locals.copy()
+            if frame.f_code is not self.__co:
+                return
+            elif event == 'call':
+                self.vars = frame.f_locals
+                if self._mod:
+                    self.vars.update((k, m(self.vars[k]))
+                                     for k, m in self._mod.items())
+            # elif event == 'return':
+            #     self._locals = frame.f_locals.copy()
 
         # tracer is activated on next call, return or exception
         sys.setprofile(tracer)
         try:
             # trace the function call
-            res = self._func(*args, **kwargs)
+            res = self.__fn(*args, **kwargs)
         finally:
             # disable tracer and replace with old one
             sys.setprofile(None)
         return res
 
     def __getitem__(self, key):
-        return self._locals[key]
+        return self.vars[key]
+
+
+# @decorator
+# def with_jumps(func):
+#     def trace(frame, event, arg):
+#         # if event == 'line':
+#         #     print(frame.f_code.co_name, frame.f_lineno)
+#         print(event, arg, frame.f_code.co_name, frame.f_lineno)
+#         if event == 'opcode':
+#             print(frame.f_code.co_name, frame.f_lineno)
+#         elif event == 'exception':
+#             e, v, tb = arg
+#             print(e, v, tb)
+#             if isinstance(v, str) and v.startswith('jump '):
+#                 frame.f_lineno += int(v.split(' ', 1)[1])
+#                 print('jumping to', frame.f_lineno)
+#         return trace
+#     def wrapper(*args, **kwargs):
+#         _trace = sys.gettrace()
+#         try:
+#             sys.settrace(trace)
+#             return func(*args, **kwargs)
+#         except AssertionError:
+#             pass
+#         finally:
+#             sys.settrace(_trace)
+#     return wrapper
 
 
 class Profile:
@@ -141,16 +195,49 @@ if __name__ == '__main__':
     import unittest
     
     class TestDecorators(unittest.TestCase):
+        # def test_with_jumps(self):
+        #     @with_jumps
+        #     def abs_(x):
+        #         if x < 0:
+        #             return -x
+        #         else:
+        #             return x
+        #     self.assertEqual(abs_(1), 1)
+        #     self.assertEqual(abs_(-1), 1)
+            
         def test_trace_locals(self):
             @trace_locals
             def f(x):
-                """ This is a test function. """
                 y = x + 1
                 return y
-            f(1)
+            self.assertEqual(f(1), 2)
             self.assertEqual(f['x'], 1)
             self.assertEqual(f['y'], 2)
-            help(f)
+            self.assertEqual(f.__name__, 'f')
             
+            @trace_locals
+            def g(y):
+                def h(x):
+                    return x + y
+                z = h(y)
+                return h(z)
+            self.assertEqual(g(2), 6)
+            self.assertEqual(g['y'], 2)
+            self.assertEqual(g['z'], 4)
+            self.assertEqual(len(g.vars), 3)
+            
+            @trace_locals(modifiers={'y': abs})
+            def h(x, y):
+                @trace_locals
+                def g(z):
+                    w = x + y + z
+                    return w
+                z = g(999)
+                return g(g['w']) - z
+            self.assertEqual(h(-1, 2), 1)
+            self.assertEqual(h['x'], -1)
+            self.assertEqual(h(1, -5), 6)
+            self.assertEqual(h['y'], 5)
+
     unittest.main()
-    
+
---
+++ my_utils/utils.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import inspect
+import unittest
+import signal
+import code
+import contextlib
+from multiprocessing import Pool
+
+
+def interact(msg=None):
+    """Start an interactive interpreter session in the current environment.
+
+    On Unix:
+      <Control>-D exits the interactive session and returns to normal execution.
+    In Windows:
+      <Control>-Z <Enter> exits the interactive session and returns to normal
+      execution.
+    """
+    # evaluate commands in current namespace
+    frame = inspect.currentframe().f_back
+    namespace = frame.f_globals.copy()
+    namespace.update(frame.f_locals)
+
+    # exit on interrupt
+    def handler(signum, frame):
+        print()
+        exit(0)
+    signal.signal(signal.SIGINT, handler)
+
+    if not msg:
+        _, filename, line, _, _, _ = inspect.stack()[1]
+        msg = 'Interacting at File "{0}", line {1} \n'.format(filename, line)
+        msg += '    Unix:    <Control>-D continues the program; \n'
+        msg += '    Windows: <Control>-Z <Enter> continues the program; \n'
+        msg += '    exit() or <Control>-C exits the program'
+
+    code.interact(msg, None, namespace)
+
+
+@contextlib.contextmanager
+def binding(**kwds):
+    "Bind global variables within a context; revert to old values on exit."
+    old_binds = {}
+    new_binds = {}
+    G = globals()
+
+    for k, v in kwds.items():
+        if k in G:
+            old_binds[k] = G[k]
+        else:
+            new_binds[k] = v
+    try:
+        G.update(kwds)
+        yield  # stuff within the context gets run here.
+    finally:  # restore
+        for k in new_binds:
+            del G[k]
+        G.update(old_binds)
+        
+
+@contextlib.contextmanager
+def jump_if(condition, lineno):
+    # def tracer(frame, event, arg):
+    #     if event == 'call':
+    #         if condition():
+    #             frame.f_lineno = lineno
+    if condition:
+        frame = sys._getframe(1)
+        breakpoint()
+        sys.settrace(jump_if)
+        try:
+            frame.f_lineno = lineno
+            yield
+        finally:
+            sys.settrace(None)
+    yield
+    
+
+def pmap(f, lst, jobs=None):
+    if jobs is None:
+        jobs = os.cpu_count()
+    l = int(len(lst) / jobs + 1 - 1e-8)
+    splits = [lst[i*l:(i+1)*l] for i in range(jobs)]
+    with Pool(jobs) as pool:
+        pool.map(f, splits)
+    
+
+def outer_frame():
+    return inspect.currentframe().f_back.f_back.f_locals
+
+
+class Default:
+    """Change to the default value if it is set to None,
+       used as a class attribute."""
+    placeholder = None
+
+    def __init__(self, default):
+        self.default = self.value = default
+
+    def __set__(self, obj, value):
+        if value is self.placeholder:
+            self.value = self.default
+        else:
+            self.value = value
+
+    def __get__(self, obj, type=None):
+        return self.value
+
+
+class IsInstance:
+
+    def __init__(self, ns=None):
+        if ns is None:
+            self.ns = globals()
+        else:
+            self.ns = ns
+
+    def __getattr__(self, type):
+        ns = super().__getattribute__('ns')
+        type = eval(type, ns)
+        if hasattr(type, '__package__'):
+            return IsInstance(type.__dict__)
+        else:
+            return lambda *args: all(isinstance(arg, type)
+                                     for arg in args)
+
+    def __call__(self, *types):
+        return lambda *args: all(isinstance(arg, types)
+                                 for arg in args)
+
+
+if __name__ == '__main__':
+    class TestJumpIf(unittest.TestCase):
+        def test_jump_to(self):
+            def abs_(x):
+                x < 0 and with_jumps(2)
+                return x
+                return -x
+            self.assertEqual(abs_(1), 1)
+            self.assertEqual(abs_(-1), 1)
+    
+    unittest.main()
+    
